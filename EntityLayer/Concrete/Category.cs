aa43a949955c4e5e10a34ad14fbd5f33ca0fda3b
Beytullah Öztürk <ozbeytullah2@outlook.com>
2022-09-21 12:11:53 +0300

    Connection String ve Context Sınıfı Eklendi

--- EntityLayer/Concrete/Category.cs
+++ EntityLayer/Concrete/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace EntityLayer.Concrete
     public class Category
     {
         // ErişimBelirleyiciTürü DeğişkenTürü DeğişkenAdı {get; set;}
+        [Key]
         public int CategoryId { get; set; }
         public string CategoryName { get; set; }
         public string CategoryDescription { get; set; }
